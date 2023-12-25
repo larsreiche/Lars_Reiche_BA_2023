724daa66b4fb153f7993e9511f1aa340d0fd274b
Lars Reiche <lars-reiche@t-online.de>
2023-12-25 22:43:40 +0100

    model concept comenting

--- Concept/model_train.py
+++ Concept/model_train.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import tensorflow as tf
 import keras
@@ -7,16 +6,18 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
-# Enviroments -----------------------------
-dataset_path = '/datasets'
+# Anwendungsparameter --------------------------------------------
+dataset_path = '/dataset'
 num_classes = 10
 training_epochs = 10
-# Definieren Sie die Bildgröße
+# Definieren der erfassten Bildgröße
 image_size = (1080 , 1920)
 
-# Datageneration -----------------------------
+# Datenanpassung und Datennormierung -----------------------------
 data_generator = ImageDataGenerator(
+    # Datennormierung
     rescale=1./255,
+    # Datenanpassung
     rotation_range=5,
     width_shift_range=20,
     height_shift_range=20,
@@ -27,14 +28,17 @@ data_generator = ImageDataGenerator(
     validation_split=0.2
 )
 
+# Generiert Datensatz für die Trainingsdaten
 train_generator = data_generator.flow_from_directory(
     dataset_path,
     target_size=image_size,
-    batch_size=32,
+    batch_size=32, 
+    # Definition d. Batchsize für das verwendete Batchlernverfahren
     class_mode='categorical',
     subset='training'
 )
 
+# Generiert Datensatz für die Validierungsdaten
 validation_generator = data_generator.flow_from_directory(
     dataset_path,
     target_size=image_size,
@@ -43,7 +47,7 @@ validation_generator = data_generator.flow_from_directory(
     subset='validation'
 )
 
-# Model -----------------------------
+# Model --------------------------------------------------------
 model = Sequential()
 # -- convolutional Layers
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(image_size[0], image_size[1], 3)))
@@ -67,6 +71,7 @@ model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Definierung und Durchführung des Trainingsablaufs
 model.fit_generator(
     train_generator,
     steps_per_epoch=train_generator.samples // train_generator.batch_size,
@@ -75,4 +80,5 @@ model.fit_generator(
     validation_steps=validation_generator.samples // validation_generator.batch_size
 )
 
+# Model wird gespeichert mit angegeben Namen
 model.save('og_model.h5')
