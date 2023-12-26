494122f55ed7f0c5b3d245428f1177fa64a4baa3
Lars Reiche <lars-reiche@t-online.de>
2023-12-26 13:29:38 +0100

    cometieren der data generation

--- BA_investigation/data_generation.py
+++ BA_investigation/data_generation.py
@@ -3,12 +3,14 @@ import cv2
 import numpy as np
 import os 
 
-urls = ["http://192.168.178.20:4747/video", "http://192.168.178.29:4747/video", "http://192.168.178.30:4747/video" ]
+# URLs zur Verwendung der verwendeten DroidCams 
+urls = ["http://172.178.178.20:4747/video", "http://172.178.178.29:4747/video", "http://172.168.178.30:4747/video" ]
+# Ordner für Datensatz der jeweiligen KLasse
 dirs = ['kampos0/zwetschgen/','kampos1/zwetschgen/', 'kampos2/zwetschgen/']
 
+# Aufnahme eines Einzelfotos
 def doFoto(url,dirname):
     cap = cv2.VideoCapture(url)
-
     if not cap.isOpened():
         print("Die Kamera konnte nicht geöffnet werden.")
     else:
@@ -21,6 +23,7 @@ def doFoto(url,dirname):
         cap.release()
     cv2.destroyAllWindows()
 
+# Erstellt passenden Dateinamen
 def createFileName(path):
     i = 0
     for osl in os.listdir(path):
@@ -28,6 +31,7 @@ def createFileName(path):
     fileName = path + str(i+1) + ".jpg"
     return fileName   
 
+# Erstellt Datensatz im passenden Verzeichnis der Kamerapositionen
 def createTrainingset():
     i = 0
     for url in urls:
@@ -35,3 +39,5 @@ def createTrainingset():
         doFoto(url,dirname)
         i = i+1 
 
+# Aufruf um ein Produkt aufzunehmen
+createTrainingset()
