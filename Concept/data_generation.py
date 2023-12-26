9202183a7b70dabb92339d9e02481f3f2a604700
Lars Reiche <lars-reiche@t-online.de>
2023-12-26 01:37:56 +0100

    comenting data generation

--- Concept/data_generation.py
+++ Concept/data_generation.py
@@ -1,22 +1,25 @@
 import cv2
 import os 
 
+# Angabe des Speicherorts der zusammelnden Bilder einer Klasse
 dir = "dataset/product_name/"
+# Angabe d. verwendeten Systemkamera
+cap = cv2.VideoCapture(0) 
 
+# Aufnahme eines Einzelfotos
 def doFoto(dirname):
-    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         print("Die Kamera konnte nicht geöffnet werden.")
     else:
         print("Kamera geöffnet")
         ret, frame = cap.read()
-
         if ret:
             cv2.imwrite(dirname, frame)
             print("Foto "+dirname+" wurde aufgenommen und gespeichert.")
         cap.release()
     cv2.destroyAllWindows()
 
+# Erstellt passenden Dateinamen
 def createFileName(dirname):
     i = 0
     for osl in os.listdir(dirname):
